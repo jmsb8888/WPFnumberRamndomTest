9f832bbbb1cd35725819f172dce872d9334302f8
jmsb8888 <jose.salamanca03@uptc.edu.co>
2024-03-20 21:08:26 -0500

    Add Controller

---
+++ numberRamndomTest/Controller/Controller.cs
@@ -0,0 +1,114 @@
+﻿using ModelRandomTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace numberRamndomTest.Controller
+{
+    internal class Controller
+    {
+        readonly MeanTest meanTest;
+        readonly VarianceTest varianceTest;
+        readonly ChiSquaredTest chiSquaredTest;
+        readonly KSTest ksTest;
+        readonly PokerTest pokerTest;
+        string filePath;
+        List<double> Data;
+        public Controller(string filePath, double EstimationError,int IntervalQuantity)
+        {
+            this.filePath = filePath;
+            this.Data = CreateCsvFile();
+            this.meanTest = new MeanTest(Data, EstimationError);
+            this.varianceTest = new VarianceTest(Data, EstimationError);
+            this.chiSquaredTest = new ChiSquaredTest(Data, EstimationError, IntervalQuantity);
+            this.ksTest = new KSTest(Data, EstimationError, IntervalQuantity);
+            this.pokerTest = new PokerTest(Data, EstimationError);
+            
+        }
+
+        private List<double> CreateCsvFile()
+        {
+            try
+            {
+                FileHandler dataFile = new FileHandler(filePath);
+                List<double> data = dataFile.ReadCsvFile();
+                return data;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error en la creación del archivo");
+                return null;
+            }
+        }
+
+        private Boolean CreateMeanTest()
+        {
+            try
+            {
+                Boolean result = meanTest.TakeTest();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error en la ejecución de la Prueba de Medias");
+                return false;
+            }
+        }
+        private Boolean CreateVarianceTest()
+        {
+            try
+            {
+                Boolean result = varianceTest.testVar();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error en la ejecución de la Prueba de Varianza");
+                return false;
+            }
+        }
+
+        private Boolean CreateCHiSquareTest()
+        {
+            try
+            {
+                Boolean result = chiSquaredTest.testChiSquarer();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error en la ejecución de la Prueba de CHI Cuadrado");
+                return false;
+            }
+        }
+        private Boolean CreateKSTest()
+        {
+            try
+            {
+                Boolean result = ksTest.testKS();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error en la ejecución de la Prueba de KS");
+                return false;
+            }
+        }
+        private Boolean CreatePokerTest()
+        {
+            try
+            {
+                Boolean result = pokerTest.testPoker();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error en la ejecución de la Prueba de Poker");
+                return false;
+            }
+        }
+    }
+}
--- numberRamndomTest/MainWindow.xaml.cs
+++ numberRamndomTest/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using ModelRandomTest;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,9 +19,18 @@ namespace numberRamndomTest
     public partial class MainWindow : Window
     {
         string filePath = string.Empty;
+        Boolean DoMeansTest = false;
+        Boolean DoVarianceTest = false;
+        Boolean DoChiSquareTest = false;
+        Boolean DoKSTest = false;
+        Boolean DoPokerTest = false;
+        Boolean DoAllTest = false;
+        Boolean isAddFile = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            ActicateAndDeactivate();
         }
 
         private void Load_File(object sender, RoutedEventArgs e)
@@ -28,34 +38,62 @@ namespace numberRamndomTest
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
             bool? result = openFileDialog.ShowDialog();
-        
+          
             if (result == true)
             {
                 filePath = openFileDialog.FileName;
                 MessageBox.Show($"Se seleccionó el archivo: {filePath}");
+                isAddFile = true;
+                ActicateAndDeactivate() ;
             }
         }
+       private void ActicateAndDeactivate()
+        {
+            btnMeans.IsEnabled = isAddFile;
+            btnVar.IsEnabled = isAddFile;
+            btnCHI.IsEnabled = isAddFile;
+            btnKS.IsEnabled = isAddFile;
+            btnPoker.IsEnabled = isAddFile;
+            btnStart.IsEnabled = isAddFile;
+            btnAll.IsEnabled = isAddFile;
 
+        }
         private void Test_Means(object sender, RoutedEventArgs e)
         {
+           
+            DoMeansTest = !DoMeansTest;
+            MessageBox.Show($"Se seleccionó el archivo: {DoMeansTest}");
 
         }
 
         private void Test_Variance(object sender, RoutedEventArgs e)
         {
-
+            DoVarianceTest = !DoVarianceTest;
         }
 
         private void Test_CHI_Square(object sender, RoutedEventArgs e)
         {
-
+            DoChiSquareTest = !DoChiSquareTest;
         }
 
         private void Test_KS(object sender, RoutedEventArgs e)
         {
-
+            DoKSTest= !DoKSTest;
         }
         private void Test_Poker(object sender, RoutedEventArgs e)
+        {
+            DoPokerTest= !DoPokerTest;
+        }
+        private void All_Test(object sender, RoutedEventArgs e)
+        {
+            bool newValue = !DoAllTest;
+            DoMeansTest = newValue;
+            DoVarianceTest = newValue;
+            DoChiSquareTest = newValue;
+            DoKSTest = newValue;
+            DoPokerTest = newValue;
+        }
+        private void Start_Test(object sender, RoutedEventArgs e)
         {
 
         }
