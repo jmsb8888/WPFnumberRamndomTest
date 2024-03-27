1bbf38c54016c753e77870a07ad2ee7e5c750ec6
jmsb8888 <jose.salamanca03@uptc.edu.co>
2024-03-27 09:25:12 -0500

    Automating error reading regardless of whether the system uses commas or periods for decimals

--- numberRamndomTest/MainWindow.xaml.cs
+++ numberRamndomTest/MainWindow.xaml.cs
@@ -98,9 +98,15 @@ namespace numberRamndomTest
             {
                 ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
                 string selectValue = selectedItem.Content.ToString().Trim();
-                if (!double.TryParse(selectValue, out Error))
+                NumberFormatInfo formatProvider = new NumberFormatInfo();
+                formatProvider.NumberDecimalSeparator = ",";
+                if (!double.TryParse(selectValue, NumberStyles.Any, formatProvider, out Error))
                 {
-                    MessageBox.Show("El valor seleccionado no es un número válido.");
+                    formatProvider.NumberDecimalSeparator = ".";
+                    if (!double.TryParse(selectValue, NumberStyles.Any, formatProvider, out Error))
+                    {
+                        MessageBox.Show("El valor seleccionado no es un número válido.");
+                    }
                 }
             }
             this.ErrorEstimated = Error;
@@ -227,7 +233,7 @@ namespace numberRamndomTest
             // Crear una instancia del controlador de prueba con la ruta del archivo, error estimado y cantidad de intervalos
             ControllerTest controller = new ControllerTest(filePath, ErrorEstimated, IntervalsQuantity);
 
-            //                  MessageBox.Show("intervalos" + IntervalsQuantity + " error " + ErrorEstimated);
+                           MessageBox.Show("intervalos" + IntervalsQuantity + " error " + ErrorEstimated);
             // Diccionario que contiene las pruebas y las funciones asociadas a ellas
             var tests = new Dictionary<string, Func<bool>>
                 {
